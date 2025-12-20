e7c76567ff600c6fcd11b4c0999f6d33762bc5dd
Suman77-stha <np03cs4a230174@heraldcollege.edu.np>
2025-12-20 23:20:32 +0545

    Delete FYP directory

--- FYP/FYP/__init__.py
+++
@@ -1,4 +0,0 @@
-import pymysql # type: ignore
-
-pymysql.install_as_MySQLdb()
-pymysql.version_info = (2, 2, 1, "final", 0)
--- FYP/FYP_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FypAppConfig(AppConfig):
-    name = 'FYP_app'
