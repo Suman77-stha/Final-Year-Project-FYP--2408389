fdda0612993c75d487629940d5ba12a1edce8a07
Suman SHrestha <np03cs4a230174@heraldcollege.edu.np>
2026-04-16 09:42:06 +0545

    Update URL routing

--- FYP_APP/urls.py
+++ FYP_APP/urls.py
@@ -4,19 +4,23 @@ from . import views
 
 urlpatterns = [
     path('Sign_Up/', views.SignUp_View, name='Sign_Up'),
+    path('verify-signup-otp/', views.verify_signup_otp, name='verify_signup_otp'),
     path('Sign_In/', views.Sign_In_view, name='Sign_In'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('', views.landing_page_view, name='Home'),
     path('About/', views.about_view, name='About'),
     path('wallet/', views.wallet_view, name='wallet'),
+    path('profile/', views.user_profile_view, name='profile'),
+    path('settings/', views.settings_view, name='settings'),
     path("get-live-price/", views.get_live_price, name="get_live_price"),
     path('AI_Assistance/', views.AI_Assistance_view, name='AI_Assistance'),
     path('api/stock-prediction/', views.stock_prediction_api, name='stock_prediction_api'),
-    path("api/profit-loss/", views.profit_loss_api, name="profit_loss_api"),
+    path('api/wallet-top5-donut/', views.wallet_top5_donut_api, name='wallet_top5_donut_api'),
     path('trading_bot/', views.trading_bot_view, name='trading_bot'),
     path("api/stock_6month_api/",views.stock_6month_api,name="stock_6month_api"),
-
+    path('chatbot/', views.Ai_Assistance_view, name='chatbot'),
+    path('chatbot/clear/', views.clear_chat, name='clear_chat'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('password_reset_done/',
          auth_views.PasswordResetDoneView.as_view(
