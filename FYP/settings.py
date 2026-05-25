274e5736713edd5d66b5a78b685eec373f3e6256
Suman SHrestha <np03cs4a230174@heraldcollege.edu.np>
2026-05-25 08:52:33 +0545

    correcting setting

--- FYP/settings.py
+++ FYP/settings.py
@@ -151,8 +151,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
