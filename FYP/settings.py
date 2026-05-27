11dcc89710f17145e395edcd81b16d99ca46afe6
Suman SHrestha <np03cs4a230174@heraldcollege.edu.np>
2026-05-27 23:32:45 +0545

    Fix SMTP config and update project files

--- FYP/settings.py
+++ FYP/settings.py
@@ -177,7 +177,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
